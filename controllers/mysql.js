4a65d2ec332b8b86a185b270c3a8d96efc8a1283
xiaoyierle <384650553@qq.com>
2019-12-10 19:23:34 +0800

    fix sql 语句增删改查

--- controllers/mysql.js
+++ controllers/mysql.js
@@ -5,9 +5,9 @@ module.exports = {
     ctx.render("mysql.html", {
       title: "测试数据库",
       name: "11111"
-    });
+    })
   },
-  "GET /mysql/getTable": async (ctx, next) => {
+  "GET /mysql/getTableData": async (ctx, next) => {
     const query = 'SELECT * FROM user'
     // 获取数据
     // let res = ctx.query; // 返回的数据格式为json
@@ -18,8 +18,66 @@ module.exports = {
         ctx.body = { message: "OK", code: '200', data: data }
         },
         () => {
-        ctx.body = { err: "数据获取失败" };
+        ctx.body = { message: "数据获取失败", code: '500' }
         }
     );
-  }
+  },
+  "POST /mysql/addUserInfo": async (ctx, next) => {
+    const { name, password } = ctx.request.body
+    const queryUser = 'SELECT * FROM user WHERE username = ? '
+    await findData(queryUser,[name]).then(
+      data => {
+        if(data && data.length){
+          ctx.body = { message: "该用户已注册", code: '500' }
+        } else {
+          const query = 'insert into user (username,password)values(?,?)'
+          // 获取数据
+          ctx.response.type = "json"
+          return addData(query,[name, password]).then(
+            data => {
+            ctx.body = { message: "OK", code: '200', data: {id:data.insertId, username: name} }
+            },
+            () => {
+            ctx.body = { message: "新增失败", code: '500' };
+            }
+          )
+        }
+      },
+      () => {
+      ctx.body = { message: "数据获取失败", code: '500' }
+      }
+    )
+  },
+  "DELETE /mysql/delUserInfo": async (ctx, next) => {
+    const { id } = ctx.request.query
+    const query = 'DELETE FROM user WHERE ??=?'
+    // 获取数据
+    // let res = ctx.query; // 返回的数据格式为json
+    ctx.response.type = "json";
+    //   let statements = res.statements;
+    await deleData(query,['id',id]).then(
+        data => {
+        ctx.body = { message: "OK", code: '200' }
+        },
+        () => {
+        ctx.body = { message: "数据获取失败", code: '500' }
+        }
+    );
+  },
+  "PUT /mysql/updateUserInfo": async (ctx, next) => {
+    const { id, password } = ctx.request.body
+    const query = 'UPDATE user SET ??=? WHERE ??=?'
+    // 获取数据
+    // let res = ctx.query; // 返回的数据格式为json
+    ctx.response.type = "json";
+    //   let statements = res.statements;
+    await exitData(query,['password',password,'id',id]).then(
+        data => {
+        ctx.body = { message: "OK", code: '200' }
+        },
+        () => {
+        ctx.body = { message: "修改失败", code: '500' }
+        }
+    );
+  },
 };
--- middleware/mysql.js
+++ middleware/mysql.js
@@ -1,14 +1,14 @@
-'use strict';
-const mysql = require( 'mysql' );
-var pool = mysql.createPool( {
-  connectionLimit : 50,
-  // host      : '172.17.0.3',
-  host      : '127.0.0.1',
-  user      : 'root',
-  password  : '123456',
-  database   : 'people_management',
-  multipleStatements : true //是否允许执行多条sql语句
-} );
+"use strict"
+const mysql = require("mysql")
+var pool = mysql.createPool({
+  connectionLimit: 50,
+  // host: '172.17.0.3',
+  host: "127.0.0.1",
+  user: "root",
+  password: "123456",
+  database: "people_management",
+  multipleStatements: true //是否允许执行多条sql语句
+})
 // 通过connection的query方法统一执行增删改查的操作。
 
 // connecQuery为增删改查方法的函数名
@@ -17,102 +17,102 @@ var pool = mysql.createPool( {
 
 // parameter 进行添加或修改的数据
 function poolFn(connecQuery, statements, parameter) {
-  　　// getConnection 创建连接池
-  　　return new Promise((resolve, reject) => {
-  　　　　pool.getConnection((err, connection) => {
-  　　　　　　if(err) {
-  　　　　　　　　throw err;
-  　　　　　　　　reject('建立连接池失败');
-  　　　　　　　　return;
-  　　　　　　}
-  　　　　　　connecQuery(connection, statements, parameter).then(data => {
-  　　　　　　　　connection.release(); // 到这步说明已经完成操作，释放连接
-  　　　　　　　　resolve(data);
-  　　　　　　});
-  　　　　});
-  　　// console.log(pool._allConnections.length); // 连接池里的连接数
-  　　});
-  }
-  /*
+  // getConnection 创建连接池
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        throw err
+        reject("建立连接池失败")
+        return
+      }
+      connecQuery(connection, statements, parameter).then(data => {
+        connection.release() // 到这步说明已经完成操作，释放连接
+        resolve(data)
+      })
+    }) // console.log(pool._allConnections.length) // 连接池里的连接数
+  })
+}
+/*
   * connection 连接句柄
   * statements 查询语句
   
   * */
-  
-  // 基于promise方法实现
-  
-  // 查询数据
-  function connecQueryFind(connection, statements) {
-  　　return new Promise((resolve, reject) => {
-  　　　　connection.query(statements, (err, result) => {
-  　　　　　　if(err) {
-  　　　　　　　　throw err;
-  　　　　　　　　reject('查询失败');
-  　　　　　　}
-  　　　　　　resolve(result);
-  　　　　});
-  　　})
-  }
-  
-  // 添加数据
-  function connecQueryAdd(connection, statements, parameter) {
-  　　return new Promise((resolve, reject) => {
-  　　　　connection.query(statements, parameter, (err, result) => {
-  　　　　　　if(err) {
-  　　　　　　　　throw err;
-  　　　　　　　　reject('添加失败');
-  　　　　　　}
-  　　　　　　resolve(result);
-  　　　　});
-  　　})
-  }
-  
-  // 删除数据
-  function connecQueryDele(connection, statements) {
-  　　　return new Promise((resolve, reject) => {
-  　　　　connection.query(statements, (err, result) => {
-  　　　　　　if(err) {
-  　　　　　　　　throw err;
-  　　　　　　　　reject('删除失败');
-  　　　　　　}
-  　　　　　　resolve(result);
-  　　　　});
-  　　})
-  }
-  
-  // 修改数据
-  function connecQueryExit(connection, statements, parameter) {
-  　　return new Promise((resolve, reject) => {
-  　　　　connection.query(statements, parameter, (err, result) => {
-  　　　　　　if(err) {
-  　　　　　　　　throw err;
-  　　　　　　　　reject('修改失败');
-  　　　　　　}
-  　　　　　　resolve(result);
-  　　　　});
-  　　})
-  }
-  // 将方法封装统一导出
+
+// 基于promise方法实现
+
+// 查询数据
+function connecQueryFind(connection, statements, parameter) {
+  return new Promise((resolve, reject) => {
+    connection.query(statements,parameter, (err, result) => {
+      if (err) {
+        throw err
+        reject("查询失败")
+      }
+      resolve(result)
+    })
+  })
+}
+
+// 添加数据
+function connecQueryAdd(connection, statements, parameter) {
+  return new Promise((resolve, reject) => {
+    connection.query(statements, parameter, (err, result) => {
+      if (err) {
+        // console.log(err,'错误错误错误')
+        throw err
+        reject("添加失败")
+      }
+      resolve(result)
+    })
+  })
+}
+
+// 删除数据
+function connecQueryDele(connection, statements, parameter) {
+  return new Promise((resolve, reject) => {
+    connection.query(statements, parameter, (err, result) => {
+      if (err) {
+        throw err
+        reject("删除失败")
+      }
+      resolve(result)
+    })
+  })
+}
+
+// 修改数据
+function connecQueryExit(connection, statements, parameter) {
+  return new Promise((resolve, reject) => {
+    connection.query(statements, parameter, (err, result) => {
+      if (err) {
+        throw err
+        reject("修改失败")
+      }
+      resolve(result)
+    })
+  })
+}
+// 将方法封装统一导出
 
 function queryFn(connecQuery, statements, parameter) {
-  　　return new Promise((resolve) => {
-  　　　　poolFn(connecQuery, statements, parameter).then(data => {
-  　　　　　　resolve(data);
-  　　　　});
-  　　});
+  return new Promise(resolve => {
+    poolFn(connecQuery, statements, parameter).then(data => {
+      resolve(data)
+    })
+  })
+}
+
+module.exports = {
+  findData(statements, parameter) {
+    return queryFn(connecQueryFind, statements, parameter)
+  },
+  addData(statements, parameter) {
+    return queryFn(connecQueryAdd, statements, parameter)
+  },
+  deleData(statements, parameter) {
+    return queryFn(connecQueryDele, statements, parameter)
+  },
+  exitData(statements, parameter) {
+    return queryFn(connecQueryExit, statements, parameter)
   }
-  
-  module.exports = {
-  　　findData(statements, parameter) {
-  　　　　return queryFn(connecQueryFind, statements, parameter);
-  　　},
-  　　addData(statements, parameter) {
-  　　　　return queryFn(connecQueryAdd, statements, parameter);
-  　　},
-  　　deleData(statements, parameter) {
-  　　　　return queryFn(connecQueryDele, statements, parameter);
-  　　},
-  　　exitData(statements, parameter) {
-  　　　　return queryFn(connecQueryExit, statements, parameter);
-  　　}
-  };
+}
