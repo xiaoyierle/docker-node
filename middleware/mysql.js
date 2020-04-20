18a5eea240f3e05a9af11dd42a342f8096287a8f
xiaoyierle <384650553@qq.com>
2020-04-21 00:57:21 +0800

    fix

--- middleware/mysql.js
+++ middleware/mysql.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 var pool = mysql.createPool({
   connectionLimit: 50,
   // host: '127.0.0.1',
-  host: "192.168.0.5",
+  host: "192.168.0.3",
   user: "root",
   password: "123456",
   database: "people_management",
